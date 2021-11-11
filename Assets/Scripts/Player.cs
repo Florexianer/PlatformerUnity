b5aed60f4a4b038e683773e4d65832498c849982
Florexianer <8111@htl.rennweg.at>
2021-11-11 11:58:10 +0100

    unfinished Jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 30.0f;
-    public float jumpVelocity = 100000f;
+    public float jumpVelocity = 10f;
     Rigidbody2D rigidbody2d;
     float horizontal;
 
     // Start is called before the first frame update
     void Start()
     {
-        //QualitySettings.vSyncCount = 0;
-        //Application.targetFrameRate = 10;
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 120;
         rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
@@ -24,20 +24,22 @@ public class Player : MonoBehaviour
     }
     void FixedUpdate()
     {
-        Vector2 position = rigidbody2d.position;
-        position.x += speed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        //Vector2 position = rigidbody2d.position;
+        //position.x += speed * Input.GetAxis("Horizontal") * Time.deltaTime;
         
         if(Input.GetButton("Jump"))
         {
             jump(1);
         }
-        rigidbody2d.MovePosition(position);
+        //rigidbody2d.MovePosition(position);
+        rigidbody2d.velocity = new Vector2(speed * Input.GetAxis("Horizontal"), rigidbody2d.velocity.y);
+
     }
 
     public void jump(int multiplicator)
     {
         Debug.Log("Jumped");
-        rigidbody2d.velocity = new Vector2(0,10000f);
+        rigidbody2d.velocity = Vector2.up * jumpVelocity * multiplicator;
     }
 
 }
